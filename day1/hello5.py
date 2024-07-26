5e55ec85e5289f44cde0223baf44a483a626f0d7
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-26 15:33:55 +0800

    Add files via upload

---
+++ day1/hello5.py
@@ -0,0 +1,15 @@
+# 變數的應用
+# 計算此次期末考的總分與平均
+chinese = 100
+math = 77
+english = 82
+# 將所有變數內容印出
+print(chinese, math, english)
+# 計算總分
+summary = chinese + math + english
+print("總分", summary)
+# 計算平均
+average = summary / 3
+print("平均", average)
+average = summary // 3  # 利用整除法
+print("平均", average)
