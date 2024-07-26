398f32479c56b2bf1e1d98c74c4876f40b70f43d
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-26 16:03:29 +0800

    Add files via upload

---
+++ day1/hello6.py
@@ -0,0 +1,15 @@
+# 變數的應用 2
+# 利用身高, 體重來計算 BMI
+h = 170  # 身高cm
+w = 60  # 體重kg
+print("身高cm", h, "體重kg", w)
+h = h/100  # 將身高從 cm 轉 m
+print("身高m", h, "體重kg", w)
+
+bmi = w / h**2  # 計算 bmi
+print("BMI", bmi)
+
+
+
+
+
