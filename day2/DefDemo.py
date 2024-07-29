6b9c570a909b9a7dcc208822901e03d062184203
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-29 16:30:15 +0800

    Add files via upload

--- day2/DefDemo.py
+++ day2/DefDemo.py
@@ -6,7 +6,8 @@ def message(name):
 
 def calc_bmi(h, w):
     bmi = w / (h/100)**2
-    print("身高:%d 體重:%d BMI:%.2f" % (h, w, bmi))
+    result = "過重" if bmi > 23 else "過輕" if bmi <= 18 else "正常"
+    print("身高:%d 體重:%d BMI:%.2f 體重%s" % (h, w, bmi, result))
 
 
 # Python 主程式
