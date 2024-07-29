d0ececb6500f27751702f9f6899d09f0ca6ab11f
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-29 14:02:03 +0800

    Add files via upload

---
+++ day2/MathDemo.py
@@ -0,0 +1,13 @@
+# 數學套件應用
+import math
+
+print(math.pi)  # 圓周率
+
+print(math.pow(2, 3))  # 次方
+print(2**3)
+
+print(math.sqrt(25))  # 開根號
+print(math.sqrt(100))  # 開根號
+print(math.sqrt(4))  # 開根號
+print(math.sqrt(16))  # 開根號
+print(math.sqrt(50))  # 開根號
