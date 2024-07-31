dd2a02a6505e9ab17ec6a0180642cea470ae1244
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-31 13:44:22 +0800

    Add files via upload

---
+++ day3/String1.py
@@ -0,0 +1,10 @@
+# 字串分析
+words = "she sell sea shell on the sea shore"
+print(words)
+print("有幾個字元(含空白):", len(words))
+print("有幾個 s 字元:", words.count("s"))
+print("有幾個 sea:", words.count("sea"))
+print("有幾個 land:", words.count("land"))
+print("sea 的位置:", words.find("sea"))
+print("land 的位置:", words.find("land"))
+
