c062b84fe1cc415c1a8f60aefdf22dc77fac490a
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-02 14:10:22 +0800

    Add files via upload

---
+++ day4/Def2.py
@@ -0,0 +1,23 @@
+# 自訂函式
+# 建立 get_bmi(h, w) 函式
+def get_bmi(h, w):
+    h = h / 100
+    bmi = w / h**2
+    return bmi
+
+
+def print_bmi(h, w):
+    h = h / 100
+    bmi = w / h**2
+    print(bmi)
+
+
+if __name__ == '__main__':
+    bmi1 = get_bmi(170, 60)
+    bmi2 = get_bmi(160, 49)
+    print("bmi1: %.1f" % bmi1)
+    print("bmi2: %.3f" % bmi2)
+    # ---------------------------
+    print_bmi(170, 60)
+    print_bmi(160, 49)
+
