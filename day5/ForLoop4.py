3a0d2cf76ffdabae554e4f3fc746ee5bdbb64660
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-05 14:29:46 +0800

    Update ForLoop4.py

--- day5/ForLoop4.py
+++ day5/ForLoop4.py
@@ -2,7 +2,7 @@
 # 自訂函式: 判斷 n 值是否是值數?
 def check_prime(n):
     # --------------------------------
-    for i in range(2, n):
+    for i in range(2, n//2):
         if n % i == 0:
             return False  # 不是質數
     # --------------------------------
