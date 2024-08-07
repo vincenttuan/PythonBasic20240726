43d9f0694e275d1c05c90715a1064418259cf831
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-07 14:51:20 +0800

    Update GUI2.py

--- day6/GUI2.py
+++ day6/GUI2.py
@@ -1,10 +1,19 @@
 import tkinter
+from tkinter import messagebox
 '''
     +--------+
     |  Label |
     | B1  B2 |
     +--------+
 '''
+# 按下 ok 鍵後要做的事
+def click_ok():
+    messagebox.showinfo("提示訊息", "Hello Python")
+
+# 按下 exit 鍵後要做的事
+def click_exit():
+    win.quit()
+
 if __name__ == '__main__':
     # 視窗建立與設定------------------------------------
     win = tkinter.Tk()
@@ -19,9 +28,9 @@ if __name__ == '__main__':
                                 width=15,          # 標籤寬度
                                 height=2)          # 標籤高度
     hello_label.pack()  # 配置到視窗中
-    ok_button = tkinter.Button(win, text="OK", font='Arial, 30')
+    ok_button = tkinter.Button(win, text="OK", font='Arial, 30', command=click_ok)
     ok_button.pack(side=tkinter.LEFT)  # 配置到視窗左側
-    exit_button = tkinter.Button(win, text="Exit", font='Arial, 30')
+    exit_button = tkinter.Button(win, text="Exit", font='Arial, 30', command=click_exit)
     exit_button.pack(side=tkinter.RIGHT)  # 配置到視窗右側
     # ------------------------------------------------
     win.mainloop()
