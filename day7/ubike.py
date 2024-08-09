fa453b3b7c4bb0d478ea524f493445abd74c0372
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-09 16:10:39 +0800

    Update ubike.py

--- day7/ubike.py
+++ day7/ubike.py
@@ -6,5 +6,11 @@ url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediat
 # 取得所有 ubike 站台列表資料
 ubike_list = json.loads(requests.get(url).text)
 print("台北市 ubike 即時資料筆數: %d" % len(ubike_list))
-print(ubike_list)  # 印出所有 ubike 資料
+#print(ubike_list)  # 印出所有 ubike 資料
+for ubike in ubike_list:
+    distance = 0
+    print("距離: %d 站名: %s 地址: %s 全部: %d 可借: %d 可還: %d" % (
+        distance, ubike['sna'], ubike['ar'], ubike['total'],
+        ubike['available_rent_bikes'], ubike['available_return_bikes']
+    ))
 
