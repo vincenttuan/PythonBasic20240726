0c5d1b7496cf07b53a5b03ff3db4025663bda315
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-09 16:25:16 +0800

    Update ubike.py

--- day7/ubike.py
+++ day7/ubike.py
@@ -1,14 +1,20 @@
 import requests
 import json
 
+from day7.Great_circle_distance import get_distance
+
 # 台北市 ubike 即時資料 json 格式
 url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
 # 取得所有 ubike 站台列表資料
 ubike_list = json.loads(requests.get(url).text)
 print("台北市 ubike 即時資料筆數: %d" % len(ubike_list))
 #print(ubike_list)  # 印出所有 ubike 資料
+# 台北市忠孝東路四段169號
+lat1, lon1 = 25.04190, 121.55050
+# --------------------------------------------------------------------------------------
 for ubike in ubike_list:
-    distance = 0
+    lat2, lon2 = ubike['latitude'], ubike['longitude']
+    distance = get_distance(lat1, lon1, lat2, lon2)
     print("距離: %d 站名: %s 地址: %s 全部: %d 可借: %d 可還: %d" % (
         distance, ubike['sna'], ubike['ar'], ubike['total'],
         ubike['available_rent_bikes'], ubike['available_return_bikes']
