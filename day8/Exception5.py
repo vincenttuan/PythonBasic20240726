3af86413db58db74095604d6d19ca1a0fb9f20b3
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-12 14:34:49 +0800

    Add files via upload

--- day8/Exception5.py
+++ day8/Exception5.py
@@ -5,11 +5,17 @@
 import math
 
 def user_input():
-    r = input('請輸入半徑:')
-    r = float(r)  # 字串轉浮點數
-    area = r ** 2 * math.pi
-    print('半徑: %.1f 圓面積: %.1f' % (r, area))
-
+    while True:
+        r = input('請輸入半徑:')
+        try:
+            r = float(r)  # 字串轉浮點數
+        except ValueError as e:
+            print("請輸入整數資料, 請重新輸入")
+            continue  # 重跑回圈
+        else:  # 沒有錯誤發生時所要執行的程式
+            area = r ** 2 * math.pi
+            print('半徑: %.1f 圓面積: %.1f' % (r, area))
+            break  # 離開迴圈
 
 if __name__ == '__main__':
     user_input()
