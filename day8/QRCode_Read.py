107a9e7cb145586023ead067ac6fd59bc7bf75a0
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-12 15:55:30 +0800

    Add files via upload

---
+++ day8/QRCode_Read.py
@@ -0,0 +1,12 @@
+import qrcode  # 生成 QRCode
+from PIL import Image  # 用於打開與處理圖像檔案
+from pyzbar.pyzbar import decode  # 用於 QRCode 解碼
+
+def read_qrcode(filename):
+    pass
+
+
+if __name__ == '__main__':
+    read_qrcode('test_qr.png')
+
+
