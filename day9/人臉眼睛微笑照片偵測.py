c773a229222fbc2590c8a2e7a550b0b206e6f226
VincentTuan <vincentjava@yahoo.com.tw>
2024-08-14 15:20:16 +0800

    Update 人臉眼睛微笑照片偵測.py

--- day9/人臉眼睛微笑照片偵測.py
+++ day9/人臉眼睛微笑照片偵測.py
@@ -16,7 +16,19 @@ frame = cv2.imread('ai/image/girl.jpg')
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 # -------------------------------------------------------------------------
+# 人臉偵測
+faces = face_cascade.detectMultiScale(
+    gray, scaleFactor=1.1, minNeighbors=15, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE
+)
 
-
+# 在 face 上畫出矩形
+for (x, y, w, h) in faces:
+    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
 # -------------------------------------------------------------------------
+
+# 顯示圖片
+cv2.imshow('My Image', frame)
+
+# 在圖片上按下任意鍵離開(例如: q)
+c = cv2.waitKey(0)
